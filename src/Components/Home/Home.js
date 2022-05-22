0c96ea2b6f1e0cb1ef1122acc19ed081a22d5baf
sufuan <abu49539@gmail.com>
2022-05-22 10:51:03 +0600

    dummy products Loaded

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
+import HomeProducts from './HomeProducts';
 
 const Home = () => {
+   const  [products, setProducts]=useProducts()
+   console.log(products);
     return (
         <div>
             <div className="relative bg-white overflow-hidden">
@@ -47,6 +51,32 @@ const Home = () => {
                     />
                 </div>
             </div>
+
+
+            {/* products  */}
+ 
+
+            <div className="bg-white">
+                    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                        <h2 className="text-2xl text-center py-5 font-bold text-gray-700">Check out what's hittin' our shelves!</h2>
+
+                        <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8">
+
+
+                            {
+
+                                products?.slice(0, 3).map(product =><HomeProducts
+                                product={product}
+                                key={product._id}
+                                ></HomeProducts>)
+                            }
+
+                        </div>
+                       
+                    </div>
+
+                </div>
+
 
             {/* stats */}
 
